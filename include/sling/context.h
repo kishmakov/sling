18c66db8cd702fc1efc0b8cf40a5610d67028d31
Kirill Shmakov <shmakir@yandex.ru>
2015-10-15 13:56:34 +0300

    Ya-based print strack trace implementation.

--- include/sling/context.h
+++ include/sling/context.h
@@ -38,4 +38,40 @@ struct Transmitter
 
 typedef std::unique_ptr<Transmitter> TransmitterUPtr;
 
+inline void print_st(const std::string& fileName)
+{
+    using namespace std;
+    ofstream fout(fileName, ios::app);
+
+   auto positions =  backtrace();
+
+   int i = 0;
+
+   for (auto position: positions) {
+        if (++i <= 2)
+            continue;
+
+        auto SL = translate(position);
+        auto functionName = SL.functionName();
+        if (functionName.find("renderer6") != string::npos) {
+            functionName = functionName.substr(functionName.find("renderer6") + 8);
+        } else if (functionName.find("tilerenderer5") != string::npos) {
+            functionName = functionName.substr(functionName.find("tilerenderer5") + 12);
+        }
+
+        if (functionName.find("(") != string::npos) {
+            functionName = functionName.substr(0, functionName.find("("));
+        }
+
+        auto fileName = SL.fileName();
+        if (fileName.rfind("/") != string::npos) {
+            fileName = fileName.substr(fileName.rfind("/") + 1);
+        }
+        fout << "        ->" << functionName << " @ " << fileName << ":" << SL.lineNumber() << endl;
+
+        if (i >= 6)
+            break;
+   }
+}
+
 } // sling
